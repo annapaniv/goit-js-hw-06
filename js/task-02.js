f82c956fae5866fe3fd329123d2dcbf60aa4584f
annapaniv <129009432+annapaniv@users.noreply.github.com>
2023-07-31 01:08:37 +0200

    load

---
+++ js/task-02.js
@@ -0,0 +1,31 @@
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+
+
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+
+const itemIngredient = ingredients.forEach(ingredient => {
+
+  const itemEl = document.createElement("li");
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+
+  const totalItemList = document.querySelector('#ingredients');
+  totalItemList.append(itemEl);
+
+
+  console.log(itemEl);
+
+});
---
+++ js/task-09.js
@@ -0,0 +1,35 @@
+// Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const el = {
+  body: document.querySelector('body'),
+  button: document.querySelector('.change-color'),
+  color: document.querySelector('.color')
+}
+
+
+el.button.addEventListener('click', onclick);
+
+function onclick(evt) {
+  el.body.style.backgroundColor = getRandomHexColor();
+  el.color.textContent = getRandomHexColor();
+}
